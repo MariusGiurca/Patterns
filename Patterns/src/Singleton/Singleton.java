bdcfc00e96b8ffe83b044a4576d569a4ce9c4f5b
Marius <Marius@DESKTOP-BQ8T4D1.local>
2017-05-10 23:58:28 +0300

    Singleton pattern

--- Patterns/src/Singleton/Singleton.java
+++ Patterns/src/Singleton/Singleton.java
@@ -1,7 +1,20 @@
 package Singleton;
 
 public class Singleton {
-	public static void main(String[] args) {
-		System.out.println("merge");
+	private static Singleton singletonInstance =  new Singleton();
+			
+	private Singleton(){
 	}
+	
+	public  static Singleton getInstance(){
+		if(singletonInstance != null){
+			return singletonInstance;
+		}
+		return  new Singleton();
+	}
+	
+	public  void message(){
+		System.out.println("I am  Singleton");
+	}
+	
 }
